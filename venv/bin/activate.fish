a15372b049eea99596d3c54c26296288e288b773
davimp3 <davi.nepomuceno@icloud.com>
2025-10-09 15:47:01 -0300

    FEAT - Atualização da Base + Leves alterações visuais.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/davi-nepomuceno/Documents/Indicadores/gs_indicadores_jira/venv
+set -gx VIRTUAL_ENV /home/davi-nepomuceno/Documents/GS-Indicadores/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
